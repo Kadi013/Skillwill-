981ef8e7e4e83371e4dda73ce4209350810e83e5
Kadi013 <demetre.tinikashvili@gmail.com>
2024-01-23 22:00:48 +0400

    first commit

---
+++ less.1/main.js
@@ -0,0 +1,36 @@
+// პირველი დავალება
+function myfun(a, b) {
+  if (a === b) {
+    console.log("ტოლია");
+  } else {
+    console.log("არ არის ტოლი");
+  }
+}
+myfun(10, 20);
+
+// მეორე დავალება
+let Fahrenheit = 89.6;
+
+function skillwill(Fahrenheit) {
+  if (Fahrenheit != Number) {
+    let Fahrenheit = 32;
+    console.log(Fahrenheit);
+  } else {
+    console.log(false);
+  }
+}
+
+skillwill(Fahrenheit);
+
+// მესამე დავალება
+
+function sweeft(a, b) {
+  if ((a, b != Number || a, b != undefined)) {
+    let operation = a * b + a / b - a;
+    console.log(operation);
+  } else {
+    console.log(false);
+  }
+}
+
+sweeft(10, 100);
